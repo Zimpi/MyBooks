8c3704b9d32a7c2115207d23ecc8491631518312
kzk1318 <jan.zimprich@kzvk.de>
2023-06-27 00:34:59 +0200

    Fix

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 using System.Security.Claims;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Responses;
 using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
 using Google.Apis.Services;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using MudBlazor.Services;
-using MyBooks;
+using Google.Apis.Util.Store;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +18,8 @@ StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configurat
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton(_ => BookServiceFactory.Initialize());
 builder.Services.AddMudServices();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddAuthentication("Cookies")
     .AddCookie(opt =>
     {
@@ -32,6 +38,40 @@ builder.Services.AddAuthentication("Cookies")
             return Task.CompletedTask;
         };
     });
+
+
+// Registriere den BookService API
+builder.Services.AddScoped<BooksService>(_ =>
+{
+    var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+        new ClientSecrets
+        {
+            ClientId = "32590108289-r2lfcelsb9e57h5ekkeri7mr62cqtqhi.apps.googleusercontent.com", 
+            ClientSecret = "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"
+        },
+        new[] { BooksService.Scope.Books },
+        "user", CancellationToken.None, new FileDataStore("Books.ListMyLibrary")).Result;
+
+    // Create the service.
+    var service = new BooksService(new BaseClientService.Initializer()
+    {
+        HttpClientInitializer = credential,
+        ApplicationName = "MyBooks",
+    });
+
+
+    return service;
+});
+
+
+
+builder.Services.AddLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+    logging.AddDebug();
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
